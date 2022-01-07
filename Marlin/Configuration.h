613689e6779fffd8560a7276ee813a9b30421848
linkus <iamaudrius@gmail.com>
2022-01-07 22:25:21 +0200

    Bed size extended + octoprint stuff added.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1390,7 +1390,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
@@ -1435,7 +1435,7 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + 15
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 245
 //#define I_MIN_POS 0
